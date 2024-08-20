bef5282647af7de3541ffa4f1777001bde24e298
josem700 <jgarciajosemanuel@gmail.com>
2024-08-20 13:40:55 +0200

    Initial Commit

---
+++ HelloCofeeApp/HelloCofeeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  HelloCofeeAppApp.swift
+//  HelloCofeeApp
+//
+//  Created by Jose M on 20/8/24.
+//
+
+import SwiftUI
+
+@main
+struct HelloCofeeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
